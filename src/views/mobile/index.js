c4aacfc0b59137f206ce919e33e77394b71b2c2f
Alexis <asantrain@tildeo.com>
2023-03-16 11:19:20 +0100

    change styles

--- src/views/mobile/index.js
+++ src/views/mobile/index.js
@@ -4,11 +4,11 @@ const Mobile = () => {
     return(
         <div className="mobile">
             <h1 className='m-3'>Saint Sebastien St Sylvestre Cappel</h1>
-            <img src="img/Blason.png" alt={'Blason de Saint Sylvestre Cappel'} className='m-5 mx-auto d-block'/>
+            <img src="img/Saint-Sébastien_Saint_Sylvestre_Cappel.png" alt={'Blason Saint-Sébastien Saint_Sylvestre_Cappel'} className='m-5 mx-auto d-block'/>
             <div className="btnGo">
-                <Link to="/Login">go!</Link>
+                <Link to="/Login">go! <div className='arrow'>-></div></Link>
             </div>
-            <img src="img/logo-archers.png" alt={'Archer'}  className='m-5 mx-auto d-block'/>
+            <img src="img/Blason.png" alt={'Blason de Saint Sylvestre Cappel'}  className='m-5 mx-auto d-block'/>
         </div>
         )
 }
