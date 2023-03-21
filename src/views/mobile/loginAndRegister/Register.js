4898c41777b6b913f74f7baa08e8b796153c56ad
Alexis <asantrain@tildeo.com>
2023-03-21 19:08:59 +0100

    change style btn and add hash password

--- src/views/mobile/loginAndRegister/Register.js
+++ src/views/mobile/loginAndRegister/Register.js
@@ -2,6 +2,7 @@ import {Form, FormGroup, Input, Button} from "reactstrap";
 import {useState} from "react";
 
 import * as User from "../../../servives/user";
+import {hashString} from "react-hash-string";
 
 const Register = () => {
     const [values, setValues] = useState({
@@ -46,7 +47,7 @@ const Register = () => {
                 "dateOfBirth": `${values.dateOfBirth}`,
                 "crew": `${values.crew}`,
                 "email": `${values.email}`,
-                "password": `${values.password}`,
+                "password": `${hashString(values.password)}`,
             }
             User.create(getData)
                 .then(() => {
