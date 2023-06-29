c884ea031fe230e279c562eaa489d739e9cf9672
Alexis <asantrain@tildeo.com>
2023-06-29 12:44:40 +0200

    refacto front

--- src/views/mobile/index.js
+++ src/views/mobile/index.js
@@ -1,11 +1,12 @@
 import {Link} from "react-router-dom";
 
 const Index = () => {
+    const isAuthenticated = localStorage.getItem('currentUser');
     return(
         <div className="mobile">
             <img src="img/Saint-Sébastien_Saint_Sylvestre_Cappel.png" alt={'Blason Saint-Sébastien Saint_Sylvestre_Cappel'} className='m-5 mx-auto d-block'/>
             <div className="btnGo">
-                <Link to="/Login">go! <div className='arrow'>-></div></Link>
+                <Link to={isAuthenticated ? "HomePage": "/Login"}>go! <div className='arrow'>-></div></Link>
             </div>
             <img src="img/Blason.png" alt={'Blason de Saint Sylvestre Cappel'}  className='m-5 mx-auto d-block'/>
         </div>
